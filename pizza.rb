daa7c86f4ccd3b60dbaa3819b0dc6943031300fd
Ricky <richardcollins52317@gmail.com>
2018-06-13 15:45:44 -0400

    Shortend code with syntax

--- pizza.rb
+++ pizza.rb
@@ -2,40 +2,30 @@ def cheese()
     cheeses = ["colby", "chedder", "parmerson", "jack"]
     cheeses[rand(0..3)]
 end
-
 def meat()
     meats = ["peperoni", "sausege", "ham", "bacon", "chicken", "ground beef"]
     meats[rand(0..5)]
 end
-
 def vegetable()
     vegetables = ["mushrooms", "pineapple", "bell peppers", "onions", "olives"]
     vegetables[rand(0..4)]
 end
-
 def size()
     sizes = ["8 inch", "16 inch", "24 inch", "32 inch", "40 inch"]
     sizes[rand(0..4)]
 end
-
 def sauce()
     sauces = ["red", "yellow", "green", "blue"]
     sauces[rand(0..3)]
 end
-
 def crust()
     crusts = ["thin crust", "stuffed crust", "deep dish", "pan"]
     crusts[rand(0..3)]
 end
-
 def make_pizza()
     puts "How many pizzas do yo want?"; print "> "; num_pizza = $stdin.gets.chomp
-
     num_pizza.to_i.times do
-        puts "Your random pizza is a #{size}, #{crust} pizza, with #{cheese}, #{meat}, #{vegetable}, and #{sauce}."
-        puts ""        
+        puts "Your random pizza is a #{size}, #{crust} pizza, with #{cheese}, #{meat}, #{vegetable}, and #{sauce}."; puts ""        
     end
 end
-
 make_pizza()
-
