cecbcc8fff09c7be7864e714aece5aa9417f6e23
Ricky <richardcollins52317@gmail.com>
2018-06-18 16:42:27 -0400

    Changed various attributes

--- pizza.rb
+++ pizza.rb
@@ -35,36 +35,52 @@ def size_price()
     [5, 9, 12, 15, 20]
 end
 def make_pizza()
-    puts "How many pizzas would you like?"; print "> "; pizza_count = $stdin.gets.chomp.to_i
+    print "How many pizzas would you like? : "; pizza_count = $stdin.gets.chomp.to_i
     while pizza_count > 0
         puts "To select your choices, enter the corresponding number. Example : 1, 2, 3, etc."
-        print "What size pizza would you like? : "
+        print "\nWhat size pizza would you like? : "
         print "#{print_size()}\n8 inch = $5, 16 inch = $9, 24 inch = $12, 32 inch = $15, 64 inch = $20 : "; size_type = $stdin.gets.chomp.to_i
-        print "What kind of crust would you like on your pizza? : "
+        
+        print "\nWhat kind of crust would you like on your pizza? : "
         print "#{print_crust()}\nThin crust = $2.50, stuffed crust = $3, deep dish = $3.50, pan = $2 : "; crust_type = $stdin.gets.chomp.to_i
-        print "What kind of cheese would you like on your pizza? : "
+        
+        print "\nWhat kind of cheese would you like on your pizza? : "
         print "#{print_cheese()}\nNo added price for mozzarella, + $0.10 for anything else : "; cheese_type = $stdin.gets.chomp.to_i
-        print "What kind of meat would you like on your pizza? : "
+        
+        print "\nWhat kind of meat would you like on your pizza? : "
         print "#{print_meat()}\nNo added price for pepperoni or sausage, ham = $0.25, bacon = $0.75, chicken = $0.50 : "; meat_type = $stdin.gets.chomp.to_i
-        print "What kind of vegetable would you like on your pizza? : "
+        
+        print "\nWhat kind of vegetable would you like on your pizza? : "
         print "#{print_vegetable()}\n+ $0.15 for any added vegtables : "; vegetable_type = $stdin.gets.chomp.to_i
-        print "What kind of sauce would you like on your pizza? : "
+        
+        print "\nWhat kind of sauce would you like on your pizza? : "
         print "#{print_sauce()}\nNo added price for marinara, + $0.15 for any other sauce : "; sauce_type = $stdin.gets.chomp.to_i
-        print "Your #{print_size[size_type - 1]}, #{print_crust[crust_type - 1]} pizza will include the following items: " 
+        
+        print "\nYour #{print_size[size_type - 1]}, #{print_crust[crust_type - 1]} pizza will include the following items: " 
         puts "#{print_cheese[cheese_type -1]} cheese, #{print_meat[meat_type - 1]}, #{print_vegetable[vegetable_type - 1]}, and #{print_sauce[sauce_type - 1]} suace." 
         pizza_count -= 1
     end
+    
     price = size_price[size_type - 1] + crust_price[crust_type - 1] + cheese_price[cheese_type - 1] + meat_price[meat_type - 1] + vegetable_price[vegetable_type - 1] + sauce_price[sauce_type - 1]
+    
     puts "Do you want that delivered for an extra $10?"; print "[y/n] : "; delivery = $stdin.gets.chomp
-    if (delivery == "y") || (delivery == "yes"); price += 10
+    
+    if (delivery == "y") || (delivery == "yes")
+        print "How many miles away do you need your pizza delivered? : "; miles = $stdin.gets.chomp.to_i
+        if miles >= 35; price += 10 + (0.20 * miles)
+        end
+    elsif miles < 35; price += 10
     else; puts "Your order will be ready in 30 minutes."
     end
+
     puts "Would you like to leave a tip?"; print "[y/n] : "; tip = $stdin.gets.chomp
+
     if (tip == "y") || (tip == "yes")
-        print "You can leave a tip ranging from 1% to 15% [Example 1, 5, 10] :"; tip_amount = $stdin.gets.chomp.to_f
+        print "You can leave a tip ranging from 1% to 15% [Example 1, 5, 10] : "; tip_amount = $stdin.gets.chomp.to_f
         tip_amount /= 100; tip_amount += 1.0
     end
-    total = (price * 1.06) * tip_amount  # first is tax, second is tip.
+
+    total = (price * 1.06) * tip_amount
     puts "That'll be $#{sprintf('%.2f', total)}, after tax."
 end
 make_pizza()
