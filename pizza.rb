636318dd1518ae01031ecc21c360fdc3090b6d62
Ricky <richardcollins52317@gmail.com>
2018-06-18 11:22:39 -0400

    added the option for tipping 1-15%

--- pizza.rb
+++ pizza.rb
@@ -21,12 +21,18 @@ def make_pizza()
     price = 0
     while pizza_count > 0
         puts "To select your choices, enter the corresponding number. Example : 1, 2, 3, etc."
-        print "What size pizza would you like? : "; print "#{print_size()}\n8 inch = $5, 16 inch = $9, 24 inch = $12, 32 inch = $15, 64 inch = $20 : "; size_type = $stdin.gets.chomp
-        print "What kind of crust would you like on your pizza? : "; print "#{print_crust()}\nThin crust = $2.50, stuffed crust = $3, deep dish = $3.50, pan = $2 : "; crust_type = $stdin.gets.chomp
-        print "What kind of cheese would you like on your pizza? : "; print "#{print_cheese()}\nNo added price for mozzarella, + $0.10 for anything else : "; cheese_type = $stdin.gets.chomp
-        print "What kind of meat would you like on your pizza? : "; print "#{print_meat()}\nNo added price for pepperoni or sausage, ham = $0.25, bacon = $0.75, chicken = $0.50 : "; meat_type = $stdin.gets.chomp
-        print "What kind of vegetable would you like on your pizza? : "; print "#{print_vegetable()}\n+ $0.15 for any added vegtables : "; vegetable_type = $stdin.gets.chomp
-        print "What kind of sauce would you like on your pizza? : "; print "#{print_sauce()}\nNo added price for marinara, + $0.15 for any other sauce : "; sauce_type = $stdin.gets.chomp
+        print "What size pizza would you like? : "
+        print "#{print_size()}\n8 inch = $5, 16 inch = $9, 24 inch = $12, 32 inch = $15, 64 inch = $20 : "; size_type = $stdin.gets.chomp
+        print "What kind of crust would you like on your pizza? : "
+        print "#{print_crust()}\nThin crust = $2.50, stuffed crust = $3, deep dish = $3.50, pan = $2 : "; crust_type = $stdin.gets.chomp
+        print "What kind of cheese would you like on your pizza? : "
+        print "#{print_cheese()}\nNo added price for mozzarella, + $0.10 for anything else : "; cheese_type = $stdin.gets.chomp
+        print "What kind of meat would you like on your pizza? : "
+        print "#{print_meat()}\nNo added price for pepperoni or sausage, ham = $0.25, bacon = $0.75, chicken = $0.50 : "; meat_type = $stdin.gets.chomp
+        print "What kind of vegetable would you like on your pizza? : "
+        print "#{print_vegetable()}\n+ $0.15 for any added vegtables : "; vegetable_type = $stdin.gets.chomp
+        print "What kind of sauce would you like on your pizza? : "
+        print "#{print_sauce()}\nNo added price for marinara, + $0.15 for any other sauce : "; sauce_type = $stdin.gets.chomp
         case crust_type.to_i
         when 1; p_crust = print_crust[0]; price += 2.50
         when 2; p_crust = print_crust[1]; price += 3
@@ -77,7 +83,12 @@ def make_pizza()
     if (delivery == "y") || (delivery == "yes"); price += 10
     else; puts "Your order will be ready in 30 minutes."
     end
-    total = (price * 1.06) # + (price * 0.15)  first is tax, second is tip.
+    puts "Would you like to leave a tip?"; print "[y/n] : "; tip = $stdin.gets.chomp
+    if (tip == "y") || (tip == "yes")
+        print "You can leave a tip ranging from 1% to 15% [Example 1, 5, 10] :"; tip_amount = $stdin.gets.chomp.to_f
+        tip_amount /= 100; tip_amount += 1.0
+    end
+    total = (price * 1.06) * tip_amount  # first is tax, second is tip.
     puts "That'll be $#{total.round(2)}, after tax."
 end
 make_pizza()
